99fd689a959878c6d15515a2968037caed5e99a3
Rhyine <rhyinestewart@gmail.com>
2021-11-15 15:17:09 -0500

    Generate user ontrollers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "users#index"
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 end
