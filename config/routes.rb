cbe251821ae75420cc4a1af404bc2537580455c4
Rhyine <rhyinestewart@gmail.com>
2021-11-15 15:18:13 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
+  resources :recipes, except: [:update] do
+    resources :foods, except: [:update]
+    end 
   get 'users/index'
   get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
