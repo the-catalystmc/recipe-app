7e5fe65bfc1a7476a3ffe163030d648c2746fce7
Rhyine <rhyinestewart@gmail.com>
2021-11-15 21:27:47 -0500

    Setup recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,40 @@
 class RecipesController < ApplicationController
+  before_action :set_user_recipes, only: %i[show update destroy]
+
   def index
+    @recipes = current_user.recipes
   end
 
-  def show
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    if current_user.recipes.create(food_params)
+      flash[:notice] = 'Recipe added successfully'
+      redirect_to recipes_url
+    else
+      flash[:error] = 'Recipe couldn\'t be added'
+      render :new
+    end
+  end
+
+  def destroy
+    if @food.destroy
+      flash[:notice] = 'Recipe deleted successfully'
+    else
+      flash[:error] = 'Recipe couldn\'t be deleted'
+    end
+    redirect_to recipes_url
+  end
+
+  private
+
+  def food_params
+    params.require(:Recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
+
+  def set_user_recipes
+    @food = current_user.recipes.find(params[:id])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  def all_recipes
+    recipe.order(created_at: :desc)
+  end
 end
