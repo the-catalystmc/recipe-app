28666f6f4cd36efd6c9f2780daf6ee017783b310
Kevin <kevinsevinche@gmail.com>
2021-11-17 10:33:59 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/new'
-  get 'foods/create'
-  get 'foods/show'
-  get 'foods/destroy'
+  # get 'recipe_foods/new'
+  # get 'recipe_foods/create'
+  # get 'recipe_foods/destroy'
+  # get 'recipe_foods/edit'
+  # get 'recipe_foods/update'
+  # get 'recipes/show'
+  # get 'foods/index'
+  # get 'foods/new'
+  # get 'foods/create'
+  # get 'foods/show'
+  # get 'foods/destroy'
   devise_for :users
   root to: "users#index"
-  resources :recipes, except: [:update] do
-    resources :foods, except: [:update]
-    end 
+  resources :recipes do
+    resources :recipe_foods, except: [:show]
+  end
   resources :foods, except: [:update, :show]
   get 'users/index'
   get 'users/show'
