b3ef781565dfbec11633507a79d24c63e09fdbec
Kevin <kevinsevinche@gmail.com>
2021-11-18 15:18:02 -0500

    Updated models validatino

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { only_integer: false }
 
   def food_quantity
     quantity = 0
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: false }
+  validates :cooking_time, presence: true, numericality: { only_integer: false }
+  validates :description, presence: true
+  validates :public, presence: true
+
 
   def all_recipes
     recipe.order(created_at: :desc)
@@ -12,5 +18,9 @@ class Recipe < ApplicationRecord
       total += ingredient.quantity * ingredient.food.price
     end
     total
+  end
+
+  def self.public_recipes
+    Recipe.where("public = true ")
   end
 end
